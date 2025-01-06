5bb3ad92d79cc713d92e8a775a00134e5ce82887
Salma atef <salma.atef20222@gmail.com>
2025-01-07 01:16:23 +0200

    new

--- script.js
+++ script.js
@@ -442,20 +442,205 @@ We work for a company building a smart home thermometer.
 Our most recent task is this: "Given an array of temperatures of one day,
 calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error.*/
 
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-function calcTempAmplitude(tmps) {
-  let max = tmps[0];
-  let min = tmps[0];
-  for (let i = 0; i < tmps.length; i++) {
-    if (typeof tmps[i] !== "number") continue;
+// function calcTempAmplitude(tmps) {
+//   let max = tmps[0];
+//   let min = tmps[0];
+//   for (let i = 0; i < tmps.length; i++) {
+//     if (typeof tmps[i] !== "number") continue;
 
-    if (tmps[i] > max) max = tmps[i];
-    if (tmps[i] < min) min = tmps[i];
-  }
-  //   console.log(max, min);
-  return max - min;
+//     if (tmps[i] > max) max = tmps[i];
+//     if (tmps[i] < min) min = tmps[i];
+//   }
+//   //   console.log(max, min);
+//   return max - min;
+// }
+// // calcTempAmplitude([1, 5, 9]);
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+/////////////////////////////////////////////////////
+//! Challenge 8
+
+/*We're building a football betting app (soccer for my American friends)! 
+Suppose we get data from a web service about a certain game ('game' variable on 
+next page). In this challenge we're gonna work with that data.*/
+
+//todo :Your tasks:
+/*
+1. Create one player array for each team (variables 'players1' and 
+  'players2') 
+  2. The first player in any player array is the goalkeeper and the others are field 
+  players. For Bayern Munich (team 1) create one variable ('gk') with the 
+  goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 
+  field players 
+  3. Create an array 'allPlayers' containing all players of both teams (22 
+  players) 
+  4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a 
+  new array ('players1Final') containing all the original team1 players plus 
+  'Thiago', 'Coutinho' and 'Perisic' 
+  5. Based on the game.odds object, create one variable for each odd (called 
+  'team1', 'draw' and 'team2') 
+  6. Write a function ('printGoals') that receives an arbitrary number of player 
+  names (not an array) and prints each of them to the console, along with the 
+  number of goals that were scored in total (number of player names passed in) 
+  7. The team with the lower odd is more likely to win. Print to the console which 
+  team is more likely to win, without using an if/else statement or the ternary 
+  operator. 
+*/
+//?Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// const [players1,players2]=game.players;
+// console.log(players1,players2);
+
+// const [gk, ...fieldPlayers] = players1;
+// const [gk2, ...fieldPlayers2] = players2;
+
+// const allPlayers = [...players1, ...players2];
+
+// const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+
+// const { team1, x:draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// function printGoals(...players) {
+//   console.log(players.length);
+// }
+// printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+// printGoals("Davies", "Muller");
+
+// printGoals(...game.scored)
+
+// team1 < team2 && console.log("team1 is more likely to win");
+
+
+////////////////////////////////////////////////////
+//! Challenge 9 
+// Let's continue with our football betting app! Keep using the 'game' variable from before. 
+/* 
+todo:  Your tasks: 
+1. Loop over the game.scored array and print each player name to the console, 
+along with the goal number (Example: "Goal 1: Lewandowski") 
+2. Use a loop to calculate the average odd and log it to the console (We already 
+studied how to calculate averages, you can go check if you don't remember) 
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this: 
+Odd of victory Bayern Munich: 1.33 
+Odd of draw: 3.25 
+Odd of victory Borrussia Dortmund: 6.5 
+
+Get the team names directly from the game object, don't hardcode them 
+(except for "draw"). Hint: Note how the odds and the game objects have the 
+same property names 
+
+4. Bonus: Create an object called 'scorers' which contains the names of the 
+players who scored as properties, and the number of goals as the value. In this 
+game, it will look like this: 
+{ 
+Gnarby: 1, 
+Hummels: 1, 
+Lewandowski: 2
+} 
+*/
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+//1- 
+for(const [i , el] of game.scored.entries()){
+  console.log(`Goal ${i+1}: ${el}`);
 }
-// calcTempAmplitude([1, 5, 9]);
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
+//2-
+const odd = Object.values(game.odds);
+// console.log(odd);
+let avg = 0;
+for (const n of odd)
+  avg += n;
+  avg /= odd.length
+  console.log(avg);
+//3- 
+const allOdds = Object.entries(game.odds);
+// console.log(allOdds);
+
+for(const [team , el] of allOdds){
+  const teamS = team === "x" ? "draw" :`victory ${game[team]}`
+  console.log(`Odd of victory ${teamS}: ${el}`);
+}
